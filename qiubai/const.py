36a141857eb0c1283c2e68a37960f219a48f8253
wangyanglong <wangyanglong@nicefilm.com>
2017-11-19 21:39:28 +0800

    更新mysql 存储

--- qiubai/const.py
+++ qiubai/const.py
@@ -4,7 +4,7 @@ SPIDER_STATUS_WORKING = 2
 SPIDER_STATUS_FINISHED = 3
 SPIDER_STATUS_FAILED = 4
 
-mongo_url = "127.0.0.1"
-mongo_port = 27017
-mongo_db = "qiubai_spider"
-mongo_col = "item"
+mysql_url = '127.0.0.1'
+mysql_user = 'wordpress'
+mysql_passwd = 'xxx'
+mysql_db = 'wordpress'
--- qiubai/pipelines.py
+++ qiubai/pipelines.py
@@ -4,23 +4,65 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
+# import pymongo
 import const
+import MySQLdb
+import datetime
+
+insert_sql ="INSERT INTO `wp_spider`(`_update`,"\
+"`_create`,`_qid`,`_author`,`_type`,`_url`,`_content`,`_pic`,`_like`,`_status`)"\
+"VALUES(%r,%r,'%s',%r,'%s','%s','%s',%r,%d,%d)"
+    
+update_sql = "UPDATE wp_spider SET `_update`=%r,"\
+"`_like`=%d WHERE `_qid`='%s'"
+
 
 class QiubaiPipeline(object):
     def __init__(self):
-        connection = pymongo.MongoClient(
-            const.mongo_url,
-            const.mongo_port
-        )
-        db = connection[const.mongo_db]
-        self.col = db[const.mongo_col]
-        self.col.ensure_index("_qid",unique=True)
+        conn = MySQLdb.connect(host=const.mysql_url,
+                                user=const.mysql_user,
+                                passwd = const.mysql_passwd,
+                                db=const.mysql_db,
+                                charset='utf8')
+        self.conn = conn
+        self.cursor = conn.cursor()
 
     def process_item(self, item, spider):
+        cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         if item['_qid'] and item['_status']:
-            self.col.update({'_qid':item['_qid']},dict(item),True)
-            return item
+            self.cursor.execute("SELECT _author FROM wp_spider WHERE _qid=%s",(item["_qid"],))
+            result = self.cursor.fetchone()
+            if not result:#insert
+                author = item['_author']
+                if author:
+                    author = author.encode('utf-8')
+                else:
+                    author = "null"
+                pic = item['_pic']
+                if pic:
+                    pic = pic.encode('utf-8')
+                else:
+                    pic = "null"
+                like = item['_like']
+                if like:
+                    like = int(like)
+                else:
+                    like = 0
+                sql = insert_sql%(cur_time,cur_time,item['_qid'].encode('utf-8'),
+                    author,item['_type'].encode('utf-8'),item['_url'].encode('utf-8'),
+                    item['_content'],pic,like,int(item['_status']))
+                print "sql",sql
+                print "end"
+                self.cursor.execute(sql)
+            else:
+                like = item['_like']
+                if like:
+                    like = int(like)
+                else:
+                    like = 0
+                sql = update_sql%(cur_time,like,item['_qid'].encode('utf-8'))
+                self.cursor.execute(sql)
+            self.conn.commit()
         else:
             raise DropItem("invalid item struct:%r",item)
         return item
